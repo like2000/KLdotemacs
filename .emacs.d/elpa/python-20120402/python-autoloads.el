a8127dc0f0a87f1a2d36fecf8ca58da9b5b0f962
Kevin Li <kli@cern.ch>
2016-02-02 12:14:09 +0100

    New version with updated helm commands and hopefully fixed python-hook for jedi.

---
+++ .emacs.d/elpa/python-20120402/python-autoloads.el
@@ -0,0 +1,29 @@
+;;; python-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "python" "python.el" (22192 35029 717413 131000))
+;;; Generated autoloads from python.el
+
+(add-to-list 'auto-mode-alist (cons (purecopy "\\.py\\'") 'python-mode))
+
+(add-to-list 'interpreter-mode-alist (cons (purecopy "python") 'python-mode))
+
+(autoload 'python-mode "python" "\
+Major mode for editing Python files.
+
+\\{python-mode-map}
+Entry to this mode calls the value of `python-mode-hook'
+if that value is non-nil.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; python-autoloads.el ends here
